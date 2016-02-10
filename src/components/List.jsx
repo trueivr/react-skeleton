dfadecba60121e564be82d7bfd9412423d1899a3
trueivr <vbdaria@hotmail.com>
2016-02-09 23:53:35 -0500

    test ready

---
+++ src/components/List.jsx
@@ -0,0 +1,21 @@
+var React = require("react");
+var ListItem = require("./ListItem.jsx");
+
+var ingredients = [
+  {"id": 1, "text": "ham"}
+  , {"id": 2, "text": "cheese"}
+  , {"id": 3, "text": "potato"}
+  , {"id": 4, "text": "tomato"}
+];
+
+var List = React.createClass({
+  render: function(){
+    var listItems = ingredients.map(function(item){
+      return <ListItem key={item.id} ingredient={item.text}/>;
+    });
+
+    return (<ul>{listItems}</ul>);
+  }
+});
+
+module.exports = List;
